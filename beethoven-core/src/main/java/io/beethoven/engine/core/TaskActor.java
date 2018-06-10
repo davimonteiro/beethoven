6c123ae2ca55a8e56b98ede49f81112abdd1eef4
Davi Monteiro Barbosa <davimonteiro.ce@gmail.com>
2018-06-10 18:25:06 -0300

    Code refactoring.

--- beethoven-core/src/main/java/io/beethoven/engine/core/TaskActor.java
+++ beethoven-core/src/main/java/io/beethoven/engine/core/TaskActor.java
@@ -1,17 +1,17 @@
 /**
  * The MIT License
  * Copyright © 2018 Davi Monteiro
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,6 +24,7 @@ package io.beethoven.engine.core;
 
 
 import akka.actor.AbstractLoggingActor;
+import akka.actor.ActorSystem;
 import akka.japi.pf.ReceiveBuilder;
 import io.beethoven.dsl.Task;
 import io.beethoven.service.TaskExecutorService;
@@ -34,6 +35,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import scala.concurrent.duration.Duration;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static akka.actor.ActorRef.noSender;
 
 /**
  * @author Davi Monteiro
